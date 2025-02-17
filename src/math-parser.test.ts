4f4bf1a2948eae9c7021be5b84bc156fe578f4ed
Alastor <6044734+Alastor-git@users.noreply.github.com>
2025-02-17 18:27:19 +0100

    fix: Added a 0-level atom to `matchGroup - Nesting` test

--- src/math-parser.test.ts
+++ src/math-parser.test.ts
@@ -931,8 +931,8 @@ test('matchGroup - Nesting', () => {
 
 test('matchGroup - Nesting', () => {
     let input: string;
-    input = '(a1(a2(a3(a4(a5(a6(a7(a8(a9(a10(a11(a12(a13(a14(a15(a16(a17(a18(a19(a20))))))))))))))))))))';
+    input = 'a0(a1(a2(a3(a4(a5(a6(a7(a8(a9(a10(a11(a12(a13(a14(a15(a16(a17(a18(a19(a20))))))))))))))))))))';
     expect(() => ParseMath.matchGroup(input)).not.toThrow(DepthLimitExceededError);
-    input = '(a1(a2(a3(a4(a5(a6(a7(a8(a9(a10(a11(a12(a13(a14(a15(a16(a17(a18(a19(a20(a21)))))))))))))))))))))';
+    input = 'a0(a1(a2(a3(a4(a5(a6(a7(a8(a9(a10(a11(a12(a13(a14(a15(a16(a17(a18(a19(a20(a21)))))))))))))))))))))';
     expect(() => ParseMath.matchGroup(input)).toThrow(DepthLimitExceededError);
 });
